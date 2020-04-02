d4b777dd90c4561f8d62c8d629851346bac26f15
CristobalLopez <clopezosses@gmail.com>
2020-04-02 11:00:07 -0300

    commit hasta video 231

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +12,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAmIQtFO0ZafFHF5VWAU3pp5ciPjHsTwlM",
+    authDomain: "firechat-7d1e6.firebaseapp.com",
+    databaseURL: "https://firechat-7d1e6.firebaseio.com",
+    projectId: "firechat-7d1e6",
+    storageBucket: "firechat-7d1e6.appspot.com",
+    messagingSenderId: "2459305175",
+    appId: "1:2459305175:web:957ad32fefc8d4c2621c92",
+    measurementId: "G-45YQHB0HYP"
+  }
 };
 
 /*
