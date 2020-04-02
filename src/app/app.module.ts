449690b3dd71977ed18160dc0f2293b3bd6a65fb
CristobalLopez <clopezosses@gmail.com>
2020-04-02 17:51:16 -0300

    commit hasta video 233 incompleto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,28 @@ import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { ChatComponent } from './components/chat/chat.component';
+
+import { ChatService } from "./providers/chat.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    ChatService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
