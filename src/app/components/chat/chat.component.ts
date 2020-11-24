c25dff0644aed732259fc039de08734b6783ab8b
CristobalLopez <clopezosses@gmail.com>
2020-11-24 20:19:38 -0300

    commit hasta video 235

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,19 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { ChatService } from '../../providers/chat.service';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styles: []
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit  {
   mensaje: string="";
+  elemento : any;
 
-  constructor() { }
+  constructor(public _cs: ChatService) { 
 
-  ngOnInit() {
+    this._cs.cargarmensajes()
+    .subscribe(()=>{
+      setTimeout(()=> this.elemento.scrollTop= this.elemento.scrollHeight, 20);
+    });
   }
+  ngOnInit(){
+    this.elemento= document.getElementById('app-mensajes');
+
+  } 
+
+
   enviar_mensaje(){
     console.log(this.mensaje);
+
+    if (this.mensaje.length==0) {
+      return;
+    }
+    this._cs.agregarMensaje(this.mensaje)
+    .then(()=>this.mensaje="")
+    .catch((err)=> console.error("error al enviar", err));
   }
 
 }
--- src/app/providers/chat.service.ts
+++ src/app/providers/chat.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Mensaje } from '../Interface/mensaje.interface';
+import { map } from 'rxjs/operators';
+import { TestBed } from '@angular/core/testing';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
 
-  constructor() { }
+  private itemsCollection: AngularFirestoreCollection<Mensaje>;
+  public chats: Mensaje[]=[];
+
+  constructor(private afs: AngularFirestore) { }
+
+  cargarmensajes(){
+
+    this.itemsCollection= this.afs.collection<Mensaje>('chats', ref=> ref.orderBy('fecha', 'desc').limit(5));
+    return this.itemsCollection.valueChanges().pipe(
+      map((mensajes:Mensaje[])=>{
+        console.log(mensajes);
+        this.chats=[];
+        for (let mensaje of mensajes) {
+          this.chats.unshift(mensaje);
+        }
+      // this.chats= mensajes;
+    }))
+    
+  }
+  agregarMensaje(texto:string){
+    
+    let mensaje :Mensaje={
+      nombre: 'Demo',
+      mensaje: texto,
+      fecha: new Date().getTime()
+    }
+
+    return this.itemsCollection.add(mensaje);
+
+  }
 }
