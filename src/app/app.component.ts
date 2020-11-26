6b5910b6f41075067e2b4035f07aab1335e71c52
CristobalLopez <clopezosses@gmail.com>
2020-11-26 00:39:12 -0300

    Final de la Seccion FireChat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+
+import { ChatService } from './providers/chat.service';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +9,6 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
   
-  public chats: Observable<any[]>;
-  constructor(firestore: AngularFirestore) {
-    this.chats = firestore.collection('chats').valueChanges();
+  constructor(public _cs: ChatService) {
   }
 }
--- src/app/providers/chat.service.ts
+++ src/app/providers/chat.service.ts
@@ -4,6 +4,9 @@ import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/fires
 import { Mensaje } from '../Interface/mensaje.interface';
 import { map } from 'rxjs/operators';
 import { TestBed } from '@angular/core/testing';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +15,21 @@ export class ChatService {
 
   private itemsCollection: AngularFirestoreCollection<Mensaje>;
   public chats: Mensaje[]=[];
+  public usuario : any ={};
 
-  constructor(private afs: AngularFirestore) { }
+  constructor(private afs: AngularFirestore, public afAuth:AngularFireAuth) {
+
+    this.afAuth.authState.subscribe( user=>{
+      console.log('Estado del Usuario: ', user);
+      if (!user) {
+        return;
+      }
+
+      this.usuario.nombre= user.displayName;
+      this.usuario.uid= user.uid;
+
+    })
+   }
 
   cargarmensajes(){
 
@@ -32,12 +48,28 @@ export class ChatService {
   agregarMensaje(texto:string){
     
     let mensaje :Mensaje={
-      nombre: 'Demo',
+      nombre: this.usuario.nombre,
       mensaje: texto,
-      fecha: new Date().getTime()
+      fecha: new Date().getTime(),
+      uid : this.usuario.uid
+      
     }
 
     return this.itemsCollection.add(mensaje);
 
+  }
+
+  login(proveedor:string){
+    if (proveedor==='google') {
+      this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+      
+    }else{
+      this.afAuth.auth.signInWithPopup(new firebase.auth.TwitterAuthProvider());
+    }
+    
+  }
+  logout(){
+    this.usuario={};
+    this.afAuth.auth.signOut();
   }
 }
